ef15baac1867c9efe7e5aa832a7323a4ca5664dc
dae1129 <127291847+dae1129@users.noreply.github.com>
2023-03-08 17:26:16 +0900

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,66 @@
+###주석
+###이것은 한줄 주석 입니다
+
+
+
+"""
+"""
+'''
+이것도 
+여러줄
+주석입니다
+'''
+
+
+### print() 함수
+print("Hello, world!")
+
+
+###type() 함수 :자료형 타입의 반환
+print(type("Hello"))    #str :string(문자열)
+print(type("1+1"))  #int : integer (정수)
+
+
+###연산자
+
+
+
+print(10/5)#나눗셈은 자료항이 float(실수)로 바뀐다
+print(19//3)#나눗셈 몫만 구하는 수
+print(19%4)#나눗셈 나머지만 구하는 수
+print(4**6)#제곱근 구하는 연산 
+print(10*3)
+
+
+
+
+###변수
+e = 10 # = : 대입연산자
+print(e)
+
+
+name = "eodus"
+age = 11
+height = 136.2
+
+
+
+print(name)
+print(age)
+print(height)
+
+
+
+###변수의 연산
+age = age+1
+age += 1 
+print(age)
+
+
+###논리 연산자
+a = 10
+b = 27698
+
+print(a<b and a==10)
+print(a=b or a<4) 
+print(a-b==5 not a=27587)
