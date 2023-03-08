d64c161bcc9735e7f1b1f9dab4a2ef93afffa860
dae1129 <127291847+dae1129@users.noreply.github.com>
2023-03-08 18:38:17 +0900

    Add files via upload

---
+++ week2.py
@@ -0,0 +1,35 @@
+'''
+print(100+ 53)
+print(2**10)
+print('10**2')
+'''
+name = 'jake'
+num = 1024
+
+
+print("4학년 4반 dae1326 입니다")
+print("%d학년 %d반 %s 입니다")
+
+print(name,num)
+# print(num+num)  
+
+
+### 포멧팅(formatting)
+#name = 'dae1326'
+#grade = 4
+#clas = 42
+
+#print("4학년 4반 dae1326 입니다")
+#print("%d학년 %d반 %s 입니다" %(grade, clas, name))
+#print("{} 학년 {}반 {} 입니다.".format(grade, clas, name)) #포멧기호
+#print(f"{grade}학년")
+#print(name,num)
+#print(num+num)
+
+
+
+
+
+###input() gkatn : 입력
+num = input("숫자를 입력하세요: ")
+#print(f "{name}님, 환영합니다~!")
